4bcc7e07697a461de25b6a1b0b867a3e9b4f3c8f
faisal-sameer <43131907+faisal-sameer@users.noreply.github.com>
2023-04-18 07:21:04 +0300

    'fi9'

--- resources/views/AboutCourse.blade.php
+++ resources/views/AboutCourse.blade.php
@@ -10,35 +10,28 @@
             </a>
             <div id="nav-search" class="nav-search is-hidden">
 
-<header class="header">
-  <div class="brand">
-    <div id="nav-burger" class="nav-burger is-active">
-      <img src="/image/hamburger-button.png" alt="Menu" class="nav-icon">
-    </div>
-    <a href="/" class="brand-logo">
-      <img id="imglogo" src="/image/logo.png" width="100" height="100" alt="">
-          </a>
-    <div id="nav-search" class="nav-search is-hidden">
-      
-      <img src="/image/cancel-button.png" alt="" id="nav-search-cancel" class="nav-icon">
-    </div>
-  </div>
-  <nav id="nav" class="nav">
-    <div id="nav-cancel" class="nav-cancel">
-      <img src="/image/cancel-button.png" alt="" class="nav-icon">
-    </div>
-    <div class="w3-dropdown-hover nav-link">
-      <img src="/image/arabic.png" width="50" height="50" alt="" class="nav-icon">
-      <div class="w3-dropdown-content w3-bar-block w3-card-4">
-         <a style="text-align: center" href="#" class="w3-bar-item w3-button">English</a>
-         <a style="text-align: center" href="#" class="w3-bar-item w3-button">عربي</a>
-       </div>
-     </div>
-    <a href="{{ route('login') }}"  class="nav-link"><h3>Log in</h3></a>
-    <a href="/" class="nav-link"><h3>Home</h3></a>
-  </nav>
-</header>
-
+                <img src="/image/cancel-button.png" alt="" id="nav-search-cancel" class="nav-icon">
+            </div>
+        </div>
+        <nav id="nav" class="nav">
+            <div id="nav-cancel" class="nav-cancel">
+                <img src="/image/cancel-button.png" alt="" class="nav-icon">
+            </div>
+            <div class="w3-dropdown-hover nav-link">
+                <img src="/image/arabic.png" width="50" height="50" alt="" class="nav-icon">
+                <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                    <a style="text-align: center" href="#" class="w3-bar-item w3-button">English</a>
+                    <a style="text-align: center" href="#" class="w3-bar-item w3-button">عربي</a>
+                </div>
+            </div>
+            <a href="{{ route('login') }}" class="nav-link">
+                <h3>Log in</h3>
+            </a>
+            <a href="/" class="nav-link">
+                <h3>Home</h3>
+            </a>
+        </nav>
+    </header>
 @endsection
 @section('content')
     <div class="container" style="direction: ltr">
--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -10,37 +10,34 @@
             </a>
             <div id="nav-search" class="nav-search is-hidden">
 
-<header class="header">
-  <div class="brand">
-    <div id="nav-burger" class="nav-burger is-active">
-      <img src="/image/hamburger-button.png" alt="Menu" class="nav-icon">
-    </div>
-    <a href="/" class="brand-logo">
-      <img id="imglogo" src="/image/logo.png" width="100" height="100" alt="">
-          </a>
-    <div id="nav-search" class="nav-search is-hidden">
-      
-      <img src="/image/cancel-button.png" alt="" id="nav-search-cancel" class="nav-icon">
-    </div>
-  </div>
-  <nav id="nav" class="nav">
-    <div id="nav-cancel" class="nav-cancel">
-      <img src="/image/cancel-button.png" alt="" class="nav-icon">
-    </div>
-    <div class="w3-dropdown-hover nav-link">
-      <img src="/image/arabic.png" width="50" height="50" alt="" class="nav-icon">
-      <div class="w3-dropdown-content w3-bar-block w3-card-4">
-         <a style="text-align: center" href="#" class="w3-bar-item w3-button">English</a>
-         <a style="text-align: center" href="#" class="w3-bar-item w3-button">عربي</a>
-       </div>
-     </div>
-    <a href="{{ route('Register') }}"  class="nav-link"><h3>Register Course</h3></a>
-     <a href="{{ route('login') }}"  class="nav-link"><h3>Log in</h3></a>
-    <a href="#about" class="nav-link"><h3>about us</h3></a>
-    <a href="#courses" class="nav-link"><h3>The Courses</h3></a>
-  </nav>
-</header>
-
+                <img src="/image/cancel-button.png" alt="" id="nav-search-cancel" class="nav-icon">
+            </div>
+        </div>
+        <nav id="nav" class="nav">
+            <div id="nav-cancel" class="nav-cancel">
+                <img src="/image/cancel-button.png" alt="" class="nav-icon">
+            </div>
+            <div class="w3-dropdown-hover nav-link">
+                <img src="/image/arabic.png" width="50" height="50" alt="" class="nav-icon">
+                <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                    <a style="text-align: center" href="#" class="w3-bar-item w3-button">English</a>
+                    <a style="text-align: center" href="#" class="w3-bar-item w3-button">عربي</a>
+                </div>
+            </div>
+            <a href="{{ route('Register') }}" class="nav-link">
+                <h3>Register Course</h3>
+            </a>
+            <a href="{{ route('login') }}" class="nav-link">
+                <h3>Log in</h3>
+            </a>
+            <a href="#about" class="nav-link">
+                <h3>about us</h3>
+            </a>
+            <a href="#courses" class="nav-link">
+                <h3>The Courses</h3>
+            </a>
+        </nav>
+    </header>
 @endsection
 @section('content')
     <div id="about" style="direction: ltr" class="container-fluid bounce-in-top ">
